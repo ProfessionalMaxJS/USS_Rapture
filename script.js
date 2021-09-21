57211b2ef78ab1d1759a1b69a4306f565427aa76
Max <max.edison@yahoo.com>
2021-09-21 19:38:33 -0400

    Added sound when banner is waved over

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
 */
 
+const sound = new Audio ("http://free-loops.com/data/mp3/99/06/3d1ab5397b3a44e4ced60ac61400.mp3");
+document.querySelector("body").appendChild(sound);
+
+
 const test = document.getElementById("popcorn");
-test.addEventListener('pointerover', function() {
-    alert('I was clicked!');
+
+test.addEventListener('pointerover', function() {  
+  sound.play();
   });
+
