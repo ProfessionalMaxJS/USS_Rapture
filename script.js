5ff00be53a06b006967a5057f940c9c81bb3966b
Max <max.edison@yahoo.com>
2021-09-21 16:32:01 -0400

    Successfully created first Alert

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/*
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+*/
+
+const test = document.getElementById("popcorn");
+test.addEventListener('pointerover', function() {
+    alert('I was clicked!');
+  });
