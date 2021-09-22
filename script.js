bc07fd45f428eb744868a9b2545c16000778e62b
Max <max.edison@yahoo.com>
2021-09-21 23:39:33 -0400

    Changed popcorn imagery (for the better)

--- script.js
+++ script.js
@@ -2,15 +2,26 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
-*/
+*/ 
+//Text added for the initial assignment
 
 const sound = new Audio ("http://free-loops.com/data/mp3/99/06/3d1ab5397b3a44e4ced60ac61400.mp3");
 document.querySelector("body").appendChild(sound);
+//"grabs" popcorn sound effect and adds it to the HTML body
 
 
-const test = document.getElementById("popcorn");
+const poppingCorn = document.getElementById("popcorn");
 
-test.addEventListener('pointerover', function() {  
-  sound.play();
+function pop(){
+  sound.play(); 
+} 
+
+poppingCorn.addEventListener('pointerover', function() {  
+  pop();
   });
 
+/*
+  function stop(){
+    sound.pause();    
+  */
+ //undeveloped idea that the sound effect should stop when the mouse is off the element
